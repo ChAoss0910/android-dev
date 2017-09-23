df9dd06395b6ff620cc39f7618791cf178f01d66
Leon_Chen <Leon_Chen@guest-wireless-upc-1607-10-120-125-127.usc.edu>
2017-09-23 10:26:52 -0700

    sc2-quiz

---
+++ SCIIQuiz/app/src/main/java/com/example/leon_chen/sciiquiz/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.leon_chen.sciiquiz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnStart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStart = (Button)findViewById(R.id.start_btn);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, Activity_2.class);
+                startActivity(intent);
+
+            }
+        });
+
+        View v = findViewById(R.id.content_1);
+        v.getBackground().setAlpha(200);
+    }
+}
